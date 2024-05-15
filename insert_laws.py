64c0a0ea53baf87c2209fb1cadab9ca0071fa8b3
Salah Eddine Oukil <slhokli2002@gmail.com>
2024-05-15 15:02:42 +0100

    First commit

---
+++ insert_laws.py
@@ -0,0 +1,91 @@
+import psycopg2
+from es_index_settings import index_settings
+from dotenv import load_dotenv
+import os
+from elasticsearch import Elasticsearch, helpers
+
+load_dotenv()
+# Establish a connection to the database
+db_conn = psycopg2.connect(
+    dbname=os.getenv("PG_DB"),
+    user=os.getenv("PG_USER"),
+    password=os.getenv("PG_PASSWORD"),
+    host=os.getenv("PG_HOST"),
+)
+
+es_client = Elasticsearch(
+    "https://localhost:9200",
+    basic_auth=("elastic", "gRakNDvDTcg+puCVov5W"),
+    verify_certs=False,
+)
+
+mappings = {
+    "properties": {
+        "content": {
+            "type": "text",
+            "fields": {"keyword": {"type": "keyword", "ignore_above": 256}},
+        },
+        "field": {
+            "type": "keyword",
+            "ignore_above": 256,
+        },
+        "journal_date": {"type": "date"},
+        "journal_num": {"type": "long"},
+        "journal_page": {"type": "long"},
+        "long_content": {
+            "type": "text",
+            "fields": {"keyword": {"type": "keyword", "ignore_above": 256}},
+        },
+        "ministry": {
+            "type": "keyword",
+            "ignore_above": 256,
+        },
+        "page_fixed": {"type": "boolean"},
+        "signature_date": {"type": "date"},
+        "text_number": {
+            "type": "keyword",
+            "ignore_above": 256,
+        },
+        "text_type": {
+            "type": "keyword",
+            "ignore_above": 256,
+        },
+    }
+}
+index_settings["mappings"] = mappings
+es_client.indices.delete(index="laws")
+es_client.indices.create(index="laws", ignore=400, body=index_settings)
+
+cur = db_conn.cursor()
+
+
+def generate_laws_data():
+    cur.execute("SELECT * FROM laws;")
+    row = cur.fetchone()
+    while row:
+        yield {
+            "_index": "laws",
+            "_id": row[0],
+            "_source": {
+                "text_type": row[1],
+                "text_number": row[2],
+                "journal_date": row[3],
+                "journal_num": row[4],
+                "journal_page": row[5],
+                "signature_date": row[6],
+                "ministry": row[7],
+                "content": row[8],
+                "field": row[9],
+                "long_content": row[10],
+                "page_fixed": row[11],
+            },
+        }
+        row = cur.fetchone()
+
+
+# index data
+helpers.bulk(es_client, generate_laws_data())
+
+# Close the cursor and connection
+cur.close()
+db_conn.close()
